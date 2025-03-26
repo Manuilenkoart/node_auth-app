3f8ba52e819ca977d012455d7f19c2e0bb58f1d3
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-03-26 16:59:40 +0200

    feat: add 404 error handling for undefined routes

--- src/index.js
+++ src/index.js
@@ -10,5 +10,9 @@ app.use(express.json());
 app.use('/', authRouter);
 app.use('/user', authMiddleware, userRouter);
 
+app.use('*', (_, res) => {
+  res.status(404).send();
+});
+
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`server listen on ${port} port`));
